875139fa9d8e1684718832effc2118f68f6cf255
Quangk17 <quangbui300323@gmail.com>
2024-09-26 00:45:41 +0700

    store

--- MERENT_API/Controllers/StoreController.cs
+++ MERENT_API/Controllers/StoreController.cs
@@ -1,6 +1,83 @@
-﻿namespace MERENT_API.Controllers
+﻿using Application.Interfaces;
+using Application.ViewModels.StoreDTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MERENT_API.Controllers
 {
-    public class StoreController
+    public class StoreController: BaseController
     {
+        private readonly IStoreService _storeService;
+        public StoreController(IStoreService storeService)
+        {
+            _storeService = storeService;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> ViewAllStore()
+        {
+            var result = await _storeService.GetStoresAsync();
+            return Ok(result);
+        }
+
+
+        [HttpGet("{name}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> SearchStoreByName(string name)
+        {
+            var result = await _storeService.SearchStoreByNameAsync(name);
+            return Ok(result);
+        }
+
+        //[Authorize (Roles = "Manager")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> CreateStore([FromBody] StoreCreateDTO createDto)
+        {
+            if (createDto == null)
+            {
+                return BadRequest();
+            }
+            var c = await _storeService.CreateStoreAsync(createDto);
+            if (!c.Success)
+            {
+                return BadRequest(c);
+            }
+            return Ok(c);
+        }
+
+        // [Authorize(Roles = "Manager")]
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateStore(int id, [FromBody] StoreUpdateDTO updateDto)
+        {
+            var c = await _storeService.UpdateStoreAsync(id, updateDto);
+            if (!c.Success)
+            {
+                return BadRequest(c);
+            }
+            return Ok(c);
+        }
+
+        //  [Authorize(Roles = "Manager, Customer")]
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> DeleteStore(int id)
+        {
+            var c = await _storeService.DeleteStoreAsync(id);
+            if (!c.Success)
+            {
+                return BadRequest(c);
+            }
+            return Ok(c);
+        }
+
     }
 }
